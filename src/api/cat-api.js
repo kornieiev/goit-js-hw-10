cc74a6bd50da9aaac4fbd3613afc60a1d5137406
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2023-06-10 13:05:45 +0200

    add breed-select

--- src/api/cat-api.js
+++ src/api/cat-api.js
@@ -1,15 +1,20 @@
-const BASE_URL = 'https://api.thecatapi.com/v1/images/search?';
+const BASE_URL = 'https://api.thecatapi.com/v1/breeds/?';
 const API_KEY =
   'live_D926Uhee2exXrC31Gao4Ns1lF8BgZxM0Gd2ta4fRblFZ0yEBKDmqC1tcn4MX6tjD';
 
-const ref = `${BASE_URL}x-api-key${API_KEY}`;
+const ref = `${BASE_URL}x-api-key=${API_KEY}`;
 console.log(ref);
 
 export function fetchBreeds(param) {
-  return fetch(``).then(resp => {
+  return fetch(ref).then(resp => {
+    console.log(resp);
+
     if (!resp.ok) {
       throw new Error(resp.statusText || resp.status);
     }
+    console.log(resp);
+
     return resp.json();
   });
+  // .then(data => console.log(data));
 }
--- src/index.js
+++ src/index.js
@@ -3,4 +3,26 @@ import { fetchBreeds } from '../src/api/cat-api';
 const selectEL = document.querySelector('.breed-select');
 console.log(selectEL);
 
-fetchBreeds();
+window.addEventListener('load', onLoad);
+
+function onLoad() {
+  fetchBreeds('cats')
+    .then(resp => {
+      const markup = makeSelectMarkup(resp);
+      addMarkup(selectEL, markup);
+    })
+    .catch(error => console.log(error.message));
+}
+
+function makeSelectMarkup(items = []) {
+  return items
+    .map(({ id, name }) => {
+      return `<option value="${id}">${name}</option>
+    `;
+    })
+    .join('');
+}
+
+function addMarkup(ref, markup) {
+  ref.innerHTML = markup;
+}
